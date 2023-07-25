730edabea40d9ccb044f569a8faeffe23ef71b72
JuicetinB <135927238+JuicetinB@users.noreply.github.com>
2023-07-24 20:37:59 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='MDX23',
+   version='1.0',
+   description='audio demixer',
+   author='Et al.',
+   author_email='none',
+   packages=['mdx23'],
+   install_requires=[
+       'numpy',
+       'soundfile',
+       'scipy',
+       'torch>=1.8.1',
+       'tqdm',
+       'librosa',
+       'demucs',
+       'onnxruntime-gpu',
+       'PyQt5',
+       'gradio',
+       'matplotlib'
+   ],
+)
